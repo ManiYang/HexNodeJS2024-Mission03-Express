74b815d96570ff8dd7c939a4dff0cb2723700f76
AlbertYang <33620464+ManiYang@users.noreply.github.com>
2024-05-04 15:43:06 +0800

    connect to MongoDB

--- app.js
+++ app.js
@@ -3,9 +3,19 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 //
-dotenv.config();
+dotenv.config({ path: './.env'});
+
+const connectionString = process.env.DATABASE.replace(
+    "<password>",
+    process.env.DATABASE_PASSWORD ??= ""
+);
+
+mongoose.connect(connectionString).then(() => {
+    console.log('successfully connected to DB');
+});
 
 //
 var postsRouter = require('./routes/posts');
